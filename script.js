a932334ca734f0981bb7227989b455c456df24c9
Mendoza Enzo <mendozawayne@itis-molinari.eu>
2024-11-08 09:48:38 +0100

    aggiunta fetch con il token

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ let places = [
        coords: [45.48760768, 9.2038215]
     }
  ];
+
+
+map = document.getElementById("map").value ;
+
+
+fetch("https://us1.locationiq.com/v1/search?key=pk.51de02ca59420a8535ff19bd59504a69&q=Piazza Del Duomo, Milano&format=json&"){
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+function renderInserimento(){
+    html_template +=`  
+        "<div class="form-group">
+              <input type="date" class="form-control" id="data" required>
+            </div>
+    ` 
+}
+
  let zoom = 12;
  let maxZoom = 19;
  let map = L.map('map').setView(places[0].coords, zoom);
@@ -23,7 +41,13 @@ let places = [
     maxZoom: maxZoom,
     attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
  }).addTo(map);
+
+ //inserimento mark 
  places.forEach((place) => {
     const marker = L.marker(place.coords).addTo(map);
     marker.bindPopup(`<b>${place.name}</b>`);
  });
+
+
+ //pk.51de02ca59420a8535ff19bd59504a69 token di locationIq
+ ` 

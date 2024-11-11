aa1bb790f2cbe8c901bd7bd3d96e0df5f8108ecc
Mendoza Enzo <mendozawayne@itis-molinari.eu>
2024-11-11 13:25:47 +0100

    finale

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-let places = [
-    {
-       name: "Piazza del Duomo",
-       coords: [45.4639102, 9.1906426]
-    },
-    {
-       name: "Darsena",
-       coords: [45.4536286, 9.1755852]
-    },
-    {
-       name: "Parco Lambro",
-       coords: [45.4968296, 9.2505173]
-    },
-    {
-       name: "Stazione Centrale",
-       coords: [45.48760768, 9.2038215]
-    }
- ];
+
 
 
 
@@ -44,17 +27,12 @@ function renderInserimento() {
 
  let zoom = 12;
  let maxZoom = 19;
- let map = L.map('map').setView(places[0].coords, zoom);
+ let map = L.map('map').setView([45.4642, 9.1900],  zoom);
  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: maxZoom,
     attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
  }).addTo(map);
 
- //inserimento mark 
- places.forEach((place) => {
-    const marker = L.marker(place.coords).addTo(map);
-    marker.bindPopup(`<b>${place.name}</b>`);
- });
 
 function addAddress(){
    const address = document.getElementById("address").value
